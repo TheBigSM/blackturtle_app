6903c608d638ee9effde5c840cb8e1ca9ab0e8f7
TheBigSM <mateja.smiljanic@gmail.com>
2025-04-26 16:56:30 +0200

    no certs

--- update-certs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo cp /etc/letsencrypt/live/theblackturtle.duckdns.org/privkey.pem ~/Documents/blackturtle_app/certs/
-sudo cp /etc/letsencrypt/live/theblackturtle.duckdns.org/fullchain.pem ~/Documents/blackturtle_app/certs/
-sudo chown mateja:mateja ~/Documents/blackturtle_app/certs/*.pem
